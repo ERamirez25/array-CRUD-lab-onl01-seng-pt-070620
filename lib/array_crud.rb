71dcb34ee91ea73db89bd87371576dcd11546fb4
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-13 00:30:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,8 @@ def create_an_empty_array
  
 end
 
-def create_an_array(earth, water, wind, fire)
-puts create_an_array = ["earth", "water", "wind", "fire"]
+def create_an_array
+ ["earth", "water", "wind", "fire"]
 end
 
 def add_element_to_end_of_array(array, element)
