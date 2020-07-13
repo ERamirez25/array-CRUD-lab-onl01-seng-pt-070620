6897cde5619e9b008bcb73c7c71eeb5ab4e6a73c
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-13 00:45:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["array", "element"]
-   add_element_to_end_of_array << "arrays!"
+   add_element_to_end_of_array 
+   << "arrays!"
   
   end
 
